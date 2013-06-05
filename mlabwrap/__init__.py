72af29e9741ccde39a7bec4dc90c6606f8dafcf7
Charl Botha <cpbotha@vxlabs.com>
2013-06-05 16:17:21 +0200

    __init__ that imports mlabwrap

---
+++ mlabwrap/__init__.py
@@ -0,0 +1,5 @@
+# Python 101 dictates that we should (almost) never do this
+# This is one of those exceptional cases
+# We want the user to be able to import the mlabwrap directory,
+# and that should act just like directly importing mlabwrap.py
+from mlabwrap import *
